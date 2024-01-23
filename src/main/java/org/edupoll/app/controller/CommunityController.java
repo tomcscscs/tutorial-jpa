23600a71d9460ec1fdca8bf4ff804a7a336b29e7
tomcscscs <bright.chan88@gmail.com>
2024-01-24 00:37:26 +0900

    마스터

--- src/main/java/org/edupoll/app/controller/CommunityController.java
+++ src/main/java/org/edupoll/app/controller/CommunityController.java
@@ -28,18 +28,16 @@ public class CommunityController {
 
 	@GetMapping("/write")
 	public String showWriteForm() {
-		
-		
+
 		return "community/write";
 	}
-	
+
 	@PostMapping("/write")
-	public String proceedWrite(@ModelAttribute WriteCommand cmd,
-			HttpServletRequest request) {
+	public String proceedWrite(@ModelAttribute WriteCommand cmd, HttpServletRequest request) {
 		Feed feed = Feed.builder()//
 				.body(cmd.getBody())//
 				.ipAddress(request.getRemoteAddr())//
-				.viewCount(0L)	// 
+				.viewCount(0L) //
 				.likeCount(0L) //
 				.password(cmd.getPassword()) //
 				.title(cmd.getTitle()) //
@@ -47,74 +45,89 @@ public class CommunityController {
 				.writer(cmd.getWriter()).build();
 
 		Feed saved = feedRepository.save(feed);
-		System.out.println(saved.getId() );
-		System.out.println(feed.getId() );
-		
+		System.out.println(saved.getId());
+		System.out.println(feed.getId());
+
 		return "redirect:/community/lists";
 	}
-	
 
 	@GetMapping("/lists")
 	public String showFeedLists(Model model) {
 		Iterable<Feed> feeds = feedRepository.findAll();
 		model.addAttribute("feeds", feeds);
-		
+
 		return "community/lists";
 	}
-	
-	
+
 	@GetMapping("/view")
 	public String showSpecificFeed(@RequestParam Integer id, Model model) {
 		Optional<Feed> optional = feedRepository.findById(id);
-		
-		if(optional.isEmpty()) 
+
+		if (optional.isEmpty())
 			return "redirect:/community/lists";
-		
+
 		Feed found = optional.get();
-		found.setViewCount(found.getViewCount()+1);
+		found.setViewCount(found.getViewCount() + 1);
 		feedRepository.save(found);
-		
+
 		model.addAttribute("feed", found);
-		
+
 		return "community/view";
 	}
-	
+
 	@GetMapping("/delete")
 	public String showDeleteForm(@RequestParam Integer id, Model model) {
 		model.addAttribute("target", id);
-		
+
 		return "community/delete";
 	}
-	
+
 	@DeleteMapping("/delete")
 	public String proceedDelete(@ModelAttribute RoleCheckCommand cmd, Model model) {
 		// System.out.println(cmd);
 		// cmd 안에 feedId, feedPassword
-		
-		
+
 		Optional<Feed> optional = feedRepository.findById(cmd.getFeedId());
-		if(optional.isEmpty()) {
+		if (optional.isEmpty()) {
 			return "community/error";
 		}
 		Feed feed = optional.get();
 		boolean role = feed.getPassword().equals(cmd.getFeedPassword());
-		if(!role) {
-			
-			return "redirect:/community/delete?id="+cmd.getFeedId();
+		if (!role) {
+
+			return "redirect:/community/delete?id=" + cmd.getFeedId();
 		}
 		// feedRepository.deleteById(cmd.getFeedId());
 		feedRepository.delete(feed);
 		return "redirect:/community/lists";
 	}
-	
-	//수정부분
+
+	// 수정부분
 	@GetMapping("/modify")
-	public String showModifyForm(@RequestParam Integer id, Model model) {
-		model.addAttribute("target", id);
-		
-		return "community/delete";
+	public String showModifyForm(Model model) {
+
+		return "community/modifyBefore";
 	}
 
+	@PostMapping("/modify")
+	public String checkConditionForModifyForm(@ModelAttribute RoleCheckCommand cmd, Model model) {
+
+		Optional<Feed> optional = feedRepository.findById(cmd.getFeedId());
+		if (optional.isEmpty()) {
+			return "community/error";
+		}
+		Feed feed = optional.get();
+		boolean role = feed.getPassword().equals(cmd.getFeedPassword());
+		if (!role) {
+			return "redirect:/community/modifyBefore?id=" + cmd.getFeedId();
+		}
+		
+		model.addAttribute("roleCheckCommand", cmd);
+		
+		return "community/modifyForm";
+
+	}
+	
 	
 
 }
